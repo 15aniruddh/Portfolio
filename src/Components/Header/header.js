5de6b55a20c00c07dcf173092ae04dc0332b49e7
Aniruddh <aniruddh1995@yahoo.com>
2026-04-14 09:51:13 +0530

    Updated the title name

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -8,7 +8,7 @@ function Header() {
   const[isOpen, setIsOpen] = useState(false);
   return (
     <div className='header'>
-        <div className='logo'>Personal Portfolio</div>
+        <div className='logo'>Aniruddh's Portfolio</div>
           <div className='menu'>
             <div className='web-menu'>
               <Web />
